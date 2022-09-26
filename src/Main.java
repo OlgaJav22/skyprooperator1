336263c8a278f9efd1a61118bf8073ee0a4f6e26
OlgaJav22 <olga.ko4erga@yandex.ru>
2022-09-26 19:56:05 +0300

    Первое задание готово!

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+       int age = 15;
+       if (age >= 18) {
+           System.out.println("Поздравляем Вас с совершенолетием!");
+       } else {
+           System.out.println("Возраст совершенолетия еще не наступил, надо подождать.");
+
+       }
+
+    }
+}
